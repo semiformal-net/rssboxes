8d68e6c22d7d33d1fac3285006de0c76333629fe
admin <admin@semiformal.net>
2023-12-29 08:30:57 -0500

    feeds-add newsletter rodeo

--- rss_config.py
+++ rss_config.py
@@ -14,8 +14,9 @@ RSS_FEEDS = [
 'https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml',
 'https://forums.redflagdeals.com/feed/forum/9',
 'https://muspy.com/feed?id=kif2s9yeljcx0rfwbbypigpbbyb71c',
-'https://www.bloomberg.com/opinion/authors/ARbTQlRLRjE/matthew-s-levine.rss',
+'https://newsletter.rodeo/feeds/wpg8rbiq4o2tvsss.xml',
 'https://kagi.com/smallweb/appreciated',
 'https://daily.jstor.org/feed',
-'https://northamerica-northeast1-rssboxes-382506.cloudfunctions.net/redrss'
+'https://northamerica-northeast1-rssboxes-382506.cloudfunctions.net/redrss',
+'https://newsletter.rodeo/feeds/074dl5kuffv304ou.xml'
 ]
