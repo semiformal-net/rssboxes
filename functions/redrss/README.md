bbfc5091bc42d646c126bfa192367aefd686973e
Paul Edwards <paul.edwards@semiformal.net>
2023-12-30 22:50:50 -0500

    improve spotify integration

--- functions/redrss/README.md
+++ functions/redrss/README.md
@@ -1,25 +1,31 @@
 # Top music rss
 
-Pulls top ten music and offers spotify links to each.
+Pulls top ten music and offers spotify links to each. The code attempts to pull an album link from spotify.
+
+## spotify integration
+
+The code uses the search api to look for an album matching the results. If it can find one, it will link to it directly, like `https://open.spotify.com/album/4EshWy8AMZAg6U0FDkDbu2`. If it doesn't get a clear result it will just link to a search url that will take the user to the results so they can (hopefully) find the album, like `https://open.spotify.com/search/artist:Neil%20Young%20album:Before%20and%20After%20year:2023`. Authorizing to the spotify api is a pain. I followed [this tutorial](I followed this tutorial: https://www.youtube.com/watch?v=ZvGnvOShStI) to get the `refresh_token`.
 
 ## Secrets
 
-This expects three secrets in the secret manager:
+This expects these secrets in the secret manager:
 
 - RSS_SRC, the site to pull top music from eg, 'https://favorite.site/ajax.php'
 - RED_API_KEY, the api key for the site eg, '1ec19bb7.e4f451de62439521ec19bb73046281'
-- baseurl the site hosting this feed (this is not used for anything important; eg, 'https://spam.com/')
+- baseurl, the site hosting this feed (this is not used for anything important; eg, 'https://spam.com/')
+- b64client, b64-encoded `client_id` and `client_secret` from a spotify app eg, 'c3BhbWFmYXNmc2FnZGdsa2Rqc2xna2pkc2xna2pkc2xrZ2pzZGdzZXJnZWVlZWVlCg='
+- refresh_token, a token to use to replace an expired api token eg, 'AQABQAiHoPL-xKUX3mmM7BK6Zf5iuV3o_R4fzFSBTMl_1Bh8tmS82HDIZrzMtYscSqfObmB_NKrKJGpQbIWW7pSZb6lYo'
 
 ## Deploy
 
 Deploy with,
 
 ```
-gcloud functions deploy redrss \ 
+gcloud functions deploy redrss \
 --no-allow-unauthenticated \
 --no-gen2 --region=northamerica-northeast1 \
 --runtime=python311 --entry-point=main \
---set-secrets=RSS_SRC=RSS_SRC:1,baseurl=baseurl:1,RED_API_KEY=RED_API_KEY:1 \
+--set-secrets=RSS_SRC=RSS_SRC:1,baseurl=baseurl:1,RED_API_KEY=RED_API_KEY:1,b64client=b64client:1,refresh_token=refresh_token:1 \
 --trigger-http
 ```
 
--- functions/redrss/main.py
+++ functions/redrss/main.py
@@ -10,9 +10,12 @@ import os
 app = Flask(__name__)
 #cache.init_app(app)
 
-RSS_SRC=os.environ['RSS_SRC']           # eg, 'https://favorite.site/ajax.php'
-RED_API_KEY=os.environ['RED_API_KEY']   # eg, '1ec19bb7.e4f451de62439521ec19bb73046281'
-baseurl=os.environ['baseurl']           # eg, 'https://spam.com/'
+RSS_SRC=os.environ['RSS_SRC']               # eg, 'https://favorite.site/ajax.php'
+RED_API_KEY=os.environ['RED_API_KEY']       # eg, '1ec19bb7.e4f451de62439521ec19bb73046281'
+baseurl=os.environ['baseurl']               # eg, 'https://spam.com/'
+# these are for spotify api access. I followed this tutorial: https://www.youtube.com/watch?v=ZvGnvOShStI
+b64client=os.environ['b64client']           # eg, 'c3BhbWFmYXNmc2FnZGdsa2Rqc2xna2pkc2xna2pkc2xrZ2pzZGdzZXJnZWVlZWVlCg='
+refresh_token=os.environ['refresh_token']   # eg, 'AQABQAiHoPL-xKUX3mmM7BK6Zf5iuV3o_R4fzFSBTMl_1Bh8tmS82HDIZrzMtYscSqfObmB_NKrKJGpQbIWW7pSZb6lYo'
 
 if not baseurl.endswith('/'):
     baseurl=baseurl+'/'
@@ -28,6 +31,34 @@ def cleanuphtml(records):
             records[r]['tags'] = [ html.unescape( t ) for t in records[r]['tags'] ]
     return records
 
+def get_spotify_token(b64client,refresh_token):
+    if not b64client or not refresh_token:
+        return None
+    token_response=requests.post('https://accounts.spotify.com/api/token',
+                        data={'refresh_token': refresh_token, 'grant_type': 'refresh_token'},
+                        headers={'content-type': 'application/x-www-form-urlencoded','Authorization': 'Basic {}'.format(b64client)})
+    if not token_response.ok:
+        return None
+    token_response_json=token_response.json()
+    if not 'access_token' in token_response_json:
+        return None
+    token=token_response_json['access_token']
+    return token
+
+def get_spotify_result_url(query,token):
+    q_response=requests.get('https://api.spotify.com/v1/search?q={}&type=album&limit=1'.format(query),
+                        headers={'Authorization': 'Bearer {}'.format(token)})
+    if not q_response.ok:
+        return None
+    q_response_json=q_response.json()
+    try:
+        album_url=q_response_json['albums']['items'][0]['external_urls']['spotify']
+    except:
+        return None
+    if not album_url.startswith('https://open.spotify.com'):
+        return None
+    return album_url
+
 #@app.route("/")
 #@cache.cached(timeout=21600) # 24hr cache
 @functions_framework.http
@@ -40,6 +71,17 @@ def main(request):
     colnames=['Cover','Album','Artist','Year','Tags','releaseType']
     records=j['response'][0]['results']
     records=cleanuphtml(records)
+
+    token=get_spotify_token(b64client,refresh_token)
+
+    for i,r in enumerate(records):
+        records[i]['link']='https://open.spotify.com/search/artist:{}%20album:{}%20year:{}'.format(r['artist'],r['groupName'],r['year'])
+        if token:
+            query='artist:{}%20album:{}%20year:{}'.format(r['artist'],r['groupName'],r['year'])
+            url=get_spotify_result_url(query,token)
+            if url:
+                records[i]['link']=url
+
     return render_template("rss.xml", records=records, releasetypes=RELEASE_TYPES, baseurl=baseurl,
                            guid='%030x' % random.randrange(16**30) )
 
